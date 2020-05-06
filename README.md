c08358ba4f01daf935c8de041e7c5110bc82df61
alejo120792 <alejo120792120792@hotmail.com>
2020-05-06 08:18:16 -0500

    Primer Commit -rest -server

---
+++ README.md
@@ -0,0 +1,23 @@
+
+
+# SOCKET-Server
+
+
+Reconstruir modulos de node
+```
+npm install
+```
+
+Generar el dist/
+
+```
+tsc -w
+```
+
+Levantar el servisor, cualquiera de los dos
+
+```
+nodemon dist/
+node dist/
+
+```
---
+++ index.ts
@@ -0,0 +1,26 @@
+import Server from "./classes/server";
+import  router  from './routes/router';
+import bodyParse from 'body-parser';
+import cors from 'cors';
+
+
+
+const server = new Server();
+
+//bodyParse
+
+server.app.use( bodyParse.urlencoded({ extended: true}));
+server.app.use( bodyParse.json());
+
+// CORS
+
+server.app.use( cors({ origin: true, credentials: true }));
+
+
+// Rutas del servisio
+server.app.use('/', router);
+
+
+server.start( () => {
+    console.log(`Servidor conectado en el puerto: ${ server.port }`);
+})
